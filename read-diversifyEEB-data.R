e2e39a2d11c36aa0fd8ab9703f4c75ab26a4a48f
Michael Koontz <mikoontz@gmail.com>
2016-03-09 10:41:28 -0800

    remove spaces from Twitter handles

--- read-diversifyEEB-data.R
+++ read-diversifyEEB-data.R
@@ -8,13 +8,14 @@ load_andor_install <- function(pkg)
   paste0(pkg, " loaded.")
 }
 
-required_packages <- c("googlesheets", "lubridate")
+required_packages <- c("googlesheets", "lubridate", "stringi")
 lapply(required_packages, load_andor_install)
 
 keyString <- "1XsnX7WXgUzGEA-1zisZC29KSFrSXKzcbakIkp1nSues"
 
 dEEBkey <- gs_key(keyString)
 dEEB <- gs_read(dEEBkey)
+dEEB <- as.data.frame(dEEB)
 
 colnames(dEEB) <- c("Timestamp", "Name", "Affiliation", "Email", "Website", "Twitter", "Current_Career_Stage", "Field", "Subfield", "Subfield_Keyword1", "Subfield_Keyword2", "Country", "Underrepresented_racial_ethnic_minority_status", "Person_with_disability", "Other_underrepresented_group", "Elaborate")
 
@@ -25,5 +26,4 @@ dEEB$Timestamp <- mdy_hms(dEEB$Timestamp)
 dEEB$Twitter[!is.na(dEEB$Twitter) & substr(dEEB$Twitter, 1, 1) != "@"] <- paste0("@", dEEB$Twitter[!is.na(dEEB$Twitter) & substr(dEEB$Twitter, 1, 1) != "@"])
 
 # Remove spaces from twitter handles
-dEEB[c(grep(" ", dEEB$Twitter), 1), "Twitter"]
-grepl(" ", dEEB[c(grep(" ", dEEB$Twitter), 1), "Twitter"])
+dEEB[grep(" ", dEEB$Twitter), "Twitter"] <- gsub("\\s", "", dEEB[grep(" ", dEEB$Twitter), "Twitter"])
