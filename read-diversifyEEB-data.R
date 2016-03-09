f1823a28770618bbddb2be57420f1196d43b8d2e
Michael Koontz <mikoontz@gmail.com>
2016-03-09 08:41:03 -0800

    convert timestamp to POSIXct objects, add @ symbol to Twitter handles, start to add code to remove spaces (usually trailing spaces) from Twitter handles

--- read-diversifyEEB-data.R
+++ read-diversifyEEB-data.R
@@ -4,13 +4,16 @@ library(lubridate)
 keyString <- "1XsnX7WXgUzGEA-1zisZC29KSFrSXKzcbakIkp1nSues"
 
 dEEBkey <- gs_key(keyString)
-
 dEEB <- gs_read(dEEBkey)
-head(dEEB)
+
 colnames(dEEB) <- c("Timestamp", "Name", "Affiliation", "Email", "Website", "Twitter", "Current_Career_Stage", "Field", "Subfield", "Subfield_Keyword1", "Subfield_Keyword2", "Country", "Underrepresented_racial_ethnic_minority_status", "Person_with_disability", "Other_underrepresented_group", "Elaborate")
 
-head(dEEB)
+# Convert timestamp to POSIXct objects
 dEEB$Timestamp <- mdy_hms(dEEB$Timestamp)
+
+# Add an @ symbol to Twitter handles that do not start with one
 dEEB$Twitter[!is.na(dEEB$Twitter) & substr(dEEB$Twitter, 1, 1) != "@"] <- paste0("@", dEEB$Twitter[!is.na(dEEB$Twitter) & substr(dEEB$Twitter, 1, 1) != "@"])
 
-dEEB[grep(" ", dEEB$Twitter), c("Name", "Twitter")]
+# Remove spaces from twitter handles
+dEEB[c(grep(" ", dEEB$Twitter), 1), "Twitter"]
+grepl(" ", dEEB[c(grep(" ", dEEB$Twitter), 1), "Twitter"])
